345efcdc20a0c03c52c58c3b5a9c1b1b4f3d6764
Георгий  Лукашов <mighty@MacBook-Air-Georgij.local>
2025-07-06 17:19:04 +0300

    Добавлен конфиг

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	"github.com/12Mighty/todo-app/pkg/handler"
 	"github.com/12Mighty/todo-app/pkg/repository"
 	"github.com/12Mighty/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config: %v", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+
 }
